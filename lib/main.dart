915ceb60ef43cec17f195f2515944b1708f8cb72
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2021-12-13 23:50:31 +0900

    feat: define chat room list page class

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
-import 'package:cloud_firestore_odm/cloud_firestore_odm.dart';
-import 'package:cloud_functions_app_sample/models/app_user/app_user.dart';
+import 'package:cloud_functions_app_sample/pages/chat_room_list/chat_room_list.dart';
 import 'package:cloud_functions_app_sample/store/store.dart';
-import 'package:cloud_functions_app_sample/theme/theme.dart';
-import 'package:cloud_functions_app_sample/utils/datetime.dart';
-import 'package:cloud_functions_app_sample/utils/snackbar.dart';
-import 'package:cloud_functions_app_sample/widgets/image_widget/cached_network_image.dart';
-import 'package:cloud_functions_app_sample/widgets/loading_widget/spinkit_circle.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
@@ -30,92 +23,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Cloud Functions app',
       theme: ThemeData.dark(),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  final store = Store();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Chats'), centerTitle: false),
-      body: FirestoreBuilder<AttendingChatRoomQuerySnapshot>(
-        // ref: attendingChatRoomsRef(store.nonNullUid),
-        ref: attendingChatRoomsRef(userId: 'EqGWv1sZAMNiVifprM5ku4DNzKu2'),
-        builder: (context, snapshot, child) {
-          if (snapshot.hasError) {
-            return const Text('Something went wrong!');
-          }
-          if (!snapshot.hasData) {
-            return const PrimarySpinkitCircle(size: 48);
-          }
-          final querySnapshot = snapshot.requireData;
-          return ListView.separated(
-            separatorBuilder: (context, index) => const Divider(height: 0),
-            itemCount: querySnapshot.docs.length,
-            itemBuilder: (context, index) {
-              final attendingChatRoom = querySnapshot.docs[index].data;
-              return InkWell(
-                onTap: () {
-                  showFloatingSnackBar(context, 'まだ何も起こりません。');
-                },
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      CircleCachedNetworkImage(
-                        radius: 48,
-                        imageURL: attendingChatRoom.imageURL ?? '',
-                      ),
-                      const Gap(16),
-                      Flexible(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: Text(
-                                    '${attendingChatRoom.name}',
-                                    style: bold16,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ),
-                                const Gap(16),
-                                Text(
-                                  dateTimeString(datetime: attendingChatRoom.updatedAt),
-                                  style: regular12,
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: Text(
-                                    attendingChatRoom.lastMessage,
-                                    style: regular12,
-                                    maxLines: 2,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ),
-                                const Gap(48),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          );
-        },
-      ),
+      home: ChatRoomListPage(),
     );
   }
 }
---
+++ lib/pages/chat_room_list/chat_room_list.dart
@@ -0,0 +1,95 @@
+import 'package:cloud_firestore_odm/cloud_firestore_odm.dart';
+import 'package:cloud_functions_app_sample/models/app_user/app_user.dart';
+import 'package:cloud_functions_app_sample/store/store.dart';
+import 'package:cloud_functions_app_sample/theme/theme.dart';
+import 'package:cloud_functions_app_sample/utils/datetime.dart';
+import 'package:cloud_functions_app_sample/utils/snackbar.dart';
+import 'package:cloud_functions_app_sample/widgets/image_widget/cached_network_image.dart';
+import 'package:cloud_functions_app_sample/widgets/loading_widget/spinkit_circle.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class ChatRoomListPage extends StatelessWidget {
+  final store = Store();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Chats'), centerTitle: false),
+      body: FirestoreBuilder<AttendingChatRoomQuerySnapshot>(
+        // ref: attendingChatRoomsRef(store.nonNullUid),
+        ref: attendingChatRoomsRef(userId: 'EqGWv1sZAMNiVifprM5ku4DNzKu2'),
+        builder: (context, snapshot, child) {
+          if (snapshot.hasError) {
+            return const Text('Something went wrong!');
+          }
+          if (!snapshot.hasData) {
+            return const PrimarySpinkitCircle(size: 48);
+          }
+          final querySnapshot = snapshot.requireData;
+          return ListView.separated(
+            separatorBuilder: (context, index) => const Divider(height: 0),
+            itemCount: querySnapshot.docs.length,
+            itemBuilder: (context, index) {
+              final attendingChatRoom = querySnapshot.docs[index].data;
+              return InkWell(
+                onTap: () {
+                  showFloatingSnackBar(context, 'まだ何も起こりません。');
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CircleCachedNetworkImage(
+                        radius: 48,
+                        imageURL: attendingChatRoom.imageURL ?? '',
+                      ),
+                      const Gap(16),
+                      Flexible(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Text(
+                                    '${attendingChatRoom.name}',
+                                    style: bold16,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                                const Gap(16),
+                                Text(
+                                  dateTimeString(datetime: attendingChatRoom.updatedAt),
+                                  style: regular12,
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Text(
+                                    attendingChatRoom.lastMessage,
+                                    style: regular12,
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                                const Gap(48),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
